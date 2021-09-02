d5d74e3b91f7efafb0a870e0e2162958a45ceb8e
kuso-megane <orikazu0824@oritawakeinoMacBook-puro.local>
2021-09-02 12:44:57 +0900

    mysql/DB/init/init_app_db.sql 仮実装

--- mysql/DB/init/init_app_db.sql
+++ mysql/DB/init/init_app_db.sql
@@ -1,3 +1,9 @@
 drop database if exists app;
 create database app;
 use app;
+
+drop TABLE if exists TestTable;
+create table TestTable(
+    id TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    name varchar(100)
+);
