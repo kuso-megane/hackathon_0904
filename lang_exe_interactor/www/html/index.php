75402a347b6d0fd50a3a151aa46e826871af9682
kuso-megane <orikazu0824@oritawakeinoMacBook-puro.local>
2021-09-05 12:33:41 +0900

    lang_exe_interactor index.php 仮実装

--- lang_exe_interactor/www/html/index.php
+++ lang_exe_interactor/www/html/index.php
@@ -1,4 +1,23 @@
 <?php
-?>
 
-<p>This is lang_exe_container</p>
+/**
+ * @param post {lang: string, code: string}
+ * 
+ * @return json {result: string}
+ */
+
+require_once '/var/www/config.php';
+
+$input = json_decode(file_get_contents('php://input'), true); //array
+
+$lang = $input['lang'];
+$code = "\"" .  $input['code'] . "\"";
+
+$docker_img = LANG_IMG[$lang];
+$exe_command = LANG_EXE_COMMANDS[$lang];
+
+$result = shell_exec('docker run --rm ' . $docker_img . $exe_command . $code);
+
+
+header("Content-type: application/json; charset=UTF-8");
+echo json_encode(['result' => $result]);
